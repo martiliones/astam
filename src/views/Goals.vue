a3a9532f9bb3e01c6f34c79816dd0841f6e3b7e1
martiliones <martiliones@yandex.com>
2021-10-02 14:47:03 +0600

    Add Goals page and more

--- src/views/Goals.vue
+++ src/views/Goals.vue
@@ -1,5 +1,56 @@
 <template>
-  <div class="px-5 dark:text-white">
-    // todo
+  <div class="container mx-auto h-max rounded-tl-xl sm:rounded-t-xl p-4 pb-6 sm:p-8 lg:p-4 lg:pb-6 xl:p-8 space-y-6">
+    <h1 class="dark:text-white text-4xl font-bold font-sans">Personal goals</h1>
+    
+    <div class="flex flex-wrap items-center gap-5">
+      <div v-for="(task, index) in tasks" :key="index" class="flex max-w-200px space-x-4 items-center w-125 transition-all cursor-pointer p-5 rounded-md py-7" :class="[task.color]">
+        <div>
+          <div class="text-xl py-2">
+            {{ task.icon }}
+          </div>
+          <div class="text-white font-sans text-lg font-medium">
+            {{ task.name }}
+          </div>
+          <div class="text-gray-100 font-sans text-sm pt-2">
+            {{ task.description }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          color: 'bg-rose-400',
+          name: 'Read 10 fictional books',
+          description: 'until Sep 25',
+          icon: '📘'
+        },
+        {
+          color: 'bg-blue-400',
+          name: 'Typing - 125 wpm (rus 10k)',
+          description: 'until May 25',
+          icon: '⚡️'
+        },
+        {
+          color: 'bg-green-400',
+          name: '2100 rating on lichess atomic',
+          description: '2 days ago',
+          icon: '🔥'
+        },
+        {
+          color: 'bg-orange-400',
+          name: 'C1 English Duolingo',
+          description: 'until 2022',
+          icon: '🚩'
+        }
+      ]
+    }
+  }
+}
+</script>
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto h-max rounded-tl-xl sm:rounded-t-xl p-4 pb-6 sm:p-8 lg:p-4 lg:pb-6 xl:p-8 space-y-6 sm:space-y-8 lg:space-y-6 xl:space-y-8">
+  <div class="container mx-auto h-max rounded-tl-xl sm:rounded-t-xl p-4 pb-6 sm:p-8 lg:p-4 lg:pb-6 xl:p-8 space-y-6">
     <div class="w-full mt-5 sm:mt-8 sm:flex sm:justify-center" v-if="!tasks.length">
       <div class="flex items-center flex-col">
         <FolderIcon/>
@@ -15,17 +15,19 @@
     </div>
 
     <h1 v-else class="dark:text-white text-4xl font-bold font-sans">All task groups</h1>
+    
+    <div class="flex flex-wrap items-center gap-10">
+      <div v-for="(task, index) in tasks" :key="index" class="flex space-x-4 items-center w-125 transition-all cursor-pointer p-5 rounded-md">
+        <div class="w-4 h-4 rounded-xl" :class="[task.color]">
 
-    <div v-for="(task, index) in tasks" :key="index" class="flex space-x-4 items-center hover:pl-4 transition-all cursor-pointer">
-      <div class="w-4 h-4 rounded-xl" :class="[task.color]">
-
-      </div>
-      <div>
-        <div class="dark:text-white font-sans">
-          {{ task.name }}
         </div>
-        <div class="text-gray-500 dark:text-gray-400 font-sans">
-          {{ task.description }}
+        <div>
+          <div class="dark:text-white font-sans">
+            {{ task.name }}
+          </div>
+          <div class="text-gray-500 dark:text-gray-400 font-sans">
+            {{ task.description }}
+          </div>
         </div>
       </div>
     </div>
@@ -41,21 +43,21 @@ export default {
   data() {
     return {
       tasks: [
-        // {
-        //   color: 'bg-pink-400',
-        //   name: 'Workout',
-        //   description: 'Get notified when someones posts a comment on a posting.'
-        // },
-        // {
-        //   color: 'bg-blue-400',
-        //   name: 'Candidates',
-        //   description: 'Get notified when a candidate applies for a job.'
-        // },
-        // {
-        //   color: 'bg-orange-400',
-        //   name: 'Offers',
-        //   description: 'Get notified when a candidate accepts or rejects an offer.'
-        // }
+        {
+          color: 'bg-pink-400',
+          name: 'Workout',
+          description: 'Get notified when someones posts a comment on a posting.'
+        },
+        {
+          color: 'bg-blue-400',
+          name: 'Candidates',
+          description: 'Get notified when a candidate applies for a job.'
+        },
+        {
+          color: 'bg-orange-400',
+          name: 'Offers',
+          description: 'Get notified when a candidate accepts or rejects an offer.'
+        }
       ]
     }
   }
